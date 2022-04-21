5f381fce487556baaedf59592312227598f0f9ea
equijoin <terry_catt@yahoo.com>
2022-04-21 16:08:22 +0100

    Update streamlit_app.py
    extra menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
+
+streamlit.header('Breakfast maneu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.text('Hrad-Boiled Free-Range Egg')
