74c91b2411f9954a6470f3ee9c3bb881ea5b59af
equijoin <terry_catt@yahoo.com>
2022-04-21 16:09:49 +0100

    Update streamlit_app.py
    fixed title typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
-streamlit.header('Breakfast maneu')
+streamlit.header('Breakfast menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
